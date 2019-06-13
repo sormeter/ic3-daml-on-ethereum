09f582de2493801b5355d84c7481ba1638aeea59
Ethan Cecchetti <ethan@cs.cornell.edu>
2019-06-13 15:24:37 -0400

    adding basic Solidity Queue contract

---
+++ solidity/Queue.sol
@@ -0,0 +1,46 @@
+pragma solidity >= 0.5.1 <0.6.0;
+
+contract DamlTransactionQueue {
+    event Request(uint id, bytes payload);
+
+    uint REGISTRATION_FEE;
+
+    bytes[] txnRequests;
+    address[] registeredAddrs;
+
+    constructor(uint regFee) public {
+        REGISTRATION_FEE = regFee;
+    }
+
+    function isRegistered(address addr) public returns (bool) {
+        for (uint i = 0; i < registeredAddrs.length; i++) {
+            if (registeredAddrs[i] == addr) return true;
+        }
+        return false;
+    }
+
+    function register() external payable {
+        if (msg.value != REGISTRATION_FEE || isRegistered(msg.sender)) revert();
+
+        registeredAddrs.push(msg.sender);
+    }
+
+    function deregister() external {
+        for (uint i = 0; i < registeredAddrs.length; i++) {
+            if (registeredAddrs[i] == msg.sender) {
+                registeredAddrs[i] = registeredAddrs[registeredAddrs.length - 1];
+                registeredAddrs.pop();
+                msg.sender.transfer(REGISTRATION_FEE);
+                return;
+            }
+        }
+    }
+
+    // Submits a new DAML transaction request and returns the index of that request.
+    function submit(bytes calldata damlTxn) external returns(uint) {
+        if (!isRegistered(msg.sender)) revert();
+
+        emit Request(txnRequests.length, damlTxn);
+        return txnRequests.push(damlTxn) - 1;
+    }
+}
