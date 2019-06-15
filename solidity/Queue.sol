4a1aaa509e9f7c4b328dbf9c69b03d8819495099
b <b@b.net>
2019-06-15 19:10:10 +0200

    [solidity|Queue] use mappings instead of arrays

--- solidity/Queue.sol
+++ solidity/Queue.sol
@@ -5,42 +5,30 @@ contract DamlTransactionQueue {
 
     uint REGISTRATION_FEE;
 
-    bytes[] txnRequests;
-    address[] registeredAddrs;
+    mapping(bytes => bool) txnRequests;
+    uint txnRequestsCounter;
+    mapping(address => bool) registeredAddrs;
 
     constructor(uint regFee) public {
         REGISTRATION_FEE = regFee;
     }
 
-    function isRegistered(address addr) public view returns (bool) {
-        for (uint i = 0; i < registeredAddrs.length; i++) {
-            if (registeredAddrs[i] == addr) return true;
-        }
-        return false;
-    }
-
     function register() external payable {
-        if (msg.value != REGISTRATION_FEE || isRegistered(msg.sender)) revert("Insufficient registration fee or already registered");
-
-        registeredAddrs.push(msg.sender);
+        if (msg.value != REGISTRATION_FEE || !registeredAddrs[msg.sender]) revert("Insufficient registration fee or already registered");
+        registeredAddrs[msg.sender] = true;
     }
 
     function deregister() external {
-        for (uint i = 0; i < registeredAddrs.length; i++) {
-            if (registeredAddrs[i] == msg.sender) {
-                registeredAddrs[i] = registeredAddrs[registeredAddrs.length - 1];
-                registeredAddrs.pop();
-                msg.sender.transfer(REGISTRATION_FEE);
-                return;
-            }
-        }
+        registeredAddrs[msg.sender] = false;
+        msg.sender.transfer(REGISTRATION_FEE);
     }
 
     // Submits a new DAML transaction request and returns the index of that request.
     function submit(bytes calldata damlTxn) external returns(uint) {
-        if (!isRegistered(msg.sender)) revert("Sender not registered");
+        if (!registeredAddrs[msg.sender]) revert("Sender not registered");
 
-        emit Request(txnRequests.length, damlTxn);
-        return txnRequests.push(damlTxn) - 1;
+        emit Request(txnRequestsCounter, damlTxn);
+        txnRequests[damlTxn] = true;
+        return ++txnRequestsCounter;
     }
 }
